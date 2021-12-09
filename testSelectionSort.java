1cdc960976664e1d36eaebce253d45f3c6b21bfc
Dylan Crompton <easyd313@gmail.com>
2021-12-08 20:50:39 -0500

    setting up positive and negative tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
+	private SelectionSort temp1;
 	@Test
 	public void test() {
 		testPositive();
@@ -15,26 +16,45 @@ public class testSelectionSort {
     }
 
     public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
+			int[] arr = new int[5];
+			arr[0] = 8;
+			arr[1] = 9;
+			arr[2] = 7;
+			arr[3] = 10;
+			arr[4] = 2;
 
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = 2;
+			Sortedarr[1] = 7;
+			Sortedarr[2] = 8;
+			Sortedarr[3] = 9;
+			Sortedarr[4] = 10;
+			SelectionSort temp1 = new SelectionSort();
 
-        /** add tests to check for this unit test **/
+
+			arr = temp1.basicSelectionSort(arr);
+
+			assertArrayEquals(Sortedarr, temp1.basicSelectionSort(arr));
     }
 
     public void testNegative(){
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = -7;
+			arr[3] = -10;
+			arr[4] = -2;
 
-        /** Test data contains negative values only **/
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -10;
+			Sortedarr[1] = -9;
+			Sortedarr[2] = -8;
+			Sortedarr[3] = -7;
+			Sortedarr[4] = -2;
+
+			SelectionSort temp1 = new SelectionSort();
+			        
+			assertArrayEquals(Sortedarr, temp1.basicSelectionSort(arr));
     }
 
     public void testMixed(){
